e865f43d73de0faf42532359c37114cf1dd08ea4
Leonard Ciobanu <Leonard.Ciobanu@softwire.com>
2025-07-08 17:26:46 +0300

    done part1 and connection works

--- src/app.ts
+++ src/app.ts
@@ -17,3 +17,57 @@ app.listen(port, () => {
  */
 app.use('/healthcheck', healthcheckRoutes);
 app.use('/books', bookRoutes);
+
+
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const Connection = require('tedious').Connection;
+
+const config = {
+    server: 'localhost',
+    options: {
+        trustServerCertificate: true
+    },
+    authentication: {
+        type: 'default',
+        options: {
+            userName: 'Leonard',
+            password: '536635',
+        },
+    },
+};
+
+const connection = new Connection(config);
+
+connection.on('connect', function (err: Error) {
+    if (err) {
+        console.log(err);
+    }
+    executeStatement();
+});
+
+connection.connect();
+
+
+
+var Request = require('tedious').Request;
+function executeStatement() {
+    let request = new Request("select * from Bookish.dbo.AUTHOR", function(err, rowCount) {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log(rowCount + ' rows');
+        // and we close the connection
+        connection.close()
+      }
+    });
+
+    request.on('row', function(columns) {
+      columns.forEach(function(column) {
+        console.log(column.value);
+      });
+    });
+
+    connection.execSql(request);
+  }
+
+// connection.
